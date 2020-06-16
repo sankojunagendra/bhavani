59582aa5127f6fa12cb5a9e5b71817d3835ea3bb
Nagendra <nagendra@regaltusk.com>
2020-06-16 12:57:20 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Jai Guru Deva Bulk SMS Services</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
