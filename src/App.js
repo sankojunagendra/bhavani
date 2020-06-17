d07bdd512ecae47c3f23f605e1af889a0c13ff69
Nagendra <nagendra@regaltusk.com>
2020-06-17 16:50:08 +0530

    Prod Build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    const { notes } = this.state;
     return(
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-l">Jai Guru Deva Bulk SMS Services</h1>
