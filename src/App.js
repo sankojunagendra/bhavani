cd4796777155638cef49cd758c1bfd2c76af96fb
Nagendra <nagendra@regaltusk.com>
2020-06-17 16:42:31 +0530

    Added Tachyons to the Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Jai Guru Deva Bulk SMS Services</p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to JaiGuruDeva Bulk SMS Services
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { notes } = this.state;
+    return(
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-l">Jai Guru Deva Bulk SMS Services</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
